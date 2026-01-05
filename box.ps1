6c0fdb7f8c13a0fe45be2338bf22ce1d2c078140
Vincent Buzzano <dev@code28.me>
2026-01-05 05:15:53 +0100

    Release AmiDevBox - 2026-01-05 05:15:53

--- box.ps1
+++ box.ps1
@@ -6,8 +6,8 @@
     Standalone box.ps1 with embedded modules
 
 .NOTES
-    Build Date: 2026-01-05 05:14:35
-    Version: 0.1.21
+    Build Date: 2026-01-05 05:15:52
+    Version: 0.1.22
 #>
 
 param(
@@ -25,7 +25,7 @@ $ErrorActionPreference = 'Stop'
 # ============================================================================
 
 # Embedded version information (injected by build script)
-$script:BoxerVersion = "0.1.21"
+$script:BoxerVersion = "0.1.22"
 
 $BaseDir = Get-Location
 $BoxDir = $null
--- boxer.ps1
+++ boxer.ps1
@@ -6,8 +6,8 @@
     Standalone boxer.ps1 with embedded modules
 
 .NOTES
-    Build Date: 2026-01-05 05:14:35
-    Version: 0.1.21
+    Build Date: 2026-01-05 05:15:52
+    Version: 0.1.22
 #>
 
 param(
@@ -25,7 +25,7 @@ $ErrorActionPreference = 'Stop'
 $script:IsEmbedded = $true
 
 # Embedded version information (injected by build script)
-$script:BoxerVersion = "0.1.21"
+$script:BoxerVersion = "0.1.22"
 
 # BEGIN boxing.ps1
 # Boxing - Common bootstrapper for boxer and box
