bb3c652bdac4721d9e1a40b078c0555b9df266a5
Vincent Buzzano <dev@code28.me>
2026-01-05 05:13:18 +0100

    Release AmiDevBox - 2026-01-05 05:13:18

--- box.ps1
+++ box.ps1
@@ -6,8 +6,8 @@
     Standalone box.ps1 with embedded modules
 
 .NOTES
-    Build Date: 2026-01-05 05:11:24
-    Version: 0.1.20
+    Build Date: 2026-01-05 05:13:17
+    Version: 0.1.21
 #>
 
 param(
@@ -25,7 +25,7 @@ $ErrorActionPreference = 'Stop'
 # ============================================================================
 
 # Embedded version information (injected by build script)
-$script:BoxerVersion = "0.1.20"
+$script:BoxerVersion = "0.1.21"
 
 $BaseDir = Get-Location
 $BoxDir = $null
