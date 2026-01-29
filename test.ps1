b122a07eb7c23ef292a80a4a01e0865a9e6ed673
Vincent Buzzano <dev@code28.me>
2026-01-29 03:40:20 +0100

    test

--- test.ps1
+++ test.ps1
@@ -1,22 +1,38 @@
 # IRM/IEX context execut Detection
 
 function Boxing-IsIrmIexContext {
-    $command = $MyInvocation.MyCommand
-    if (-not $command) { return $false }
-    if ($command.Definition) {
-        $command = $command.Definition
-    } elseif ($command.ScriptBlock) {
-        $command = $command.ScriptBlock
-    } else {
+<#
+    .SYNOPSIS
+        Detects if the current script is being executed in an IRM/IEX context.
+
+    .PARAMETER MyCommand
+        MyInvocation.MyCommand object representing the current command.
+#>
+    param(
+        [Parameter(Mandatory = $false)]
+        [Object]$MyCommand = $null
+    )
+
+    Write-Host "command -> $Mycommand"
+    if (-not $Mycommand) {
+        Write-Host "no command - false"
         return $false
     }
-    #$command = "irm https://raw.githubusercontent.com/vbuzzano/AmiDevBox/refs/heads/main/test.ps1 | iex"
 
-    return $command -match '(?i)^(irm)\s+http[s]?://[^\s]+\s+\|\s+(iex).*$'
+
+    $definition = $Mycommand.Definition
+    #$definition = "irm https://raw.githubusercontent.com/vbuzzano/AmiDevBox/refs/heads/main/test.ps1 | iex"
+
+    if ($definition -and ($definition -match '(?i)^(irm)\s+http[s]?://[^\s]+\s+\|\s+(iex).*$')) {
+        Write-Host "matched - true"
+        return $true
+    }
+    Write-Host "no Definition or ScriptBlock - false"
+    return $false
 }
 
 Write-Host ""
 Write-Host "=== IRM/IEX Context Detection ==="
 Write-Host ""
-Write-Host "isIrmIEx: $(Boxing-IsIrmIexContext)"
+Write-Host "isIrmIEx: $(Boxing-IsIrmIexContext -MyCommand $MyInvocation.MyCommand)"
 Write-Host ""
