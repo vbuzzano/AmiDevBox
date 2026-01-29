68c1c5f2c1d055170bc83c63238afa329f5b0ab6
Vincent Buzzano <dev@code28.me>
2026-01-29 02:39:50 +0100

    test irm context

---
+++ test.ps1
@@ -0,0 +1,19 @@
+# IRM/IEX context execut Detection
+
+Write-Host ""
+Write-Host "=== IRM/IEX Context Detection ==="
+Write-Host ""
+Write-Host "PSScriptRoot: $PSScriptRoot"
+Write-Host "------------------------------"
+Write-Host "IsIrmIexContext variable exists: $(Get-Variable -Name IsIrmIexContext -Scope Script -ErrorAction SilentlyContinue -ne $null)"
+Write-Host "--> Object:"
+Get-Variable -Name IsIrmIexContext -Scope Script
+Write-Host "------------------------------"
+
+# IRM/IEX context execut Detection
+$script:IsIrmIex = (-not $PSScriptRoot) -or (Get-Variable -Name IsIrmIexContext -Scope Script -ValueOnly -ErrorAction SilentlyContinue)
+echo "IsIrmIex: $script:IsIrmIex"
+
+exit 0
+
+
